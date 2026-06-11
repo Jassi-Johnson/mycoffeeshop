c715359aa387ff32a60fda880d9bdea2225ec195
Jassi Johnson <jassijohnson191@gmail.com>
2026-06-10 22:32:11 -0500

    fixed seperator

--- src/components/ui/Separator.jsx
+++ src/components/ui/Separator.jsx
@@ -1,5 +1,11 @@
 import { cn } from "../../lib/utils";
-
+ 
 export default function Separator({ className, ...props }) {
-  return <div className={cn("h-px w-full bg-white/20", className)} {...props} />;
+  return (
+    <div
+      className={cn("h-px w-full bg-gradient-to-r from-transparent via-white/20 to-transparent", className)}
+      role="separator"
+      {...props}
+    />
+  );
 }
